6ddfc27462097d2a4ffd12e3c57a956844262422
nirmalizaap@gmail.com <passme123!@#>
2018-01-12 19:00:09 +0530

    send mail corrections

--- application/controllers/service.php
+++ application/controllers/service.php
@@ -534,7 +534,7 @@ class Service extends REST_Controller
 
 					           $this->email->clear(TRUE);
 					           $this->email->set_newline("\r\n");		
-                     $this->email->from('allivizhi@izaaptech.in','Allivizhi');
+                     $this->email->from('contact@heresmygps.com','Contact');
                      $this->email->to($email);
                      $this->email->subject('Forgot Password');
                      $this->email->message($message);
