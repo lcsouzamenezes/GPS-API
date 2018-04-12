6df998862264b01d88aade780ea0aac80ef8aae4
Punitha <punitha.lakshmi87@gmail.com>
2018-04-12 17:25:10 +0530

    -

--- application/controllers/service.php
+++ application/controllers/service.php
@@ -1843,7 +1843,7 @@ class Service extends REST_Controller
                     }
                 }                
 
-                return $this->response(array('status' =>'success','join_key' => $join_key,'user_id'=>$user_id,'type'=>$result['type'],'description'=>$grp_descript,'location_type'=>$grp_loctype,'lat'=>$grp_lat,'lon'=>$grp_lon,'date_created'=>$grp_datecreate,'is_view'=>$res['is_view'],'date_update' => $grp_dateupdate ,'members' => $user, 'static_maps' => $static_maps), 200);
+                return $this->response(array('status' =>'success','group_id' => $result['id'],'join_key' => $join_key,'user_id'=>$user_id,'type'=>$result['type'],'description'=>$grp_descript,'location_type'=>$grp_loctype,'lat'=>$grp_lat,'lon'=>$grp_lon,'date_created'=>$grp_datecreate,'is_view'=>$res['is_view'],'date_update' => $grp_dateupdate ,'members' => $user, 'static_maps' => $static_maps), 200);
             }
             else
             {
@@ -3910,7 +3910,6 @@ class Service extends REST_Controller
       $image         = $this->get("image_path");
       $link_message  = $this->get("link_message");
       $notification_image = $this->get("fileName");
-      $filetype = $this->get->('filetype');
 
       $group_data    = $this->db->query("select * from groups where join_key='".$joinKey."'")->row_array();
       $user_details  = $this->user_groups_model->get_user_gcm($group_data['id']);
@@ -3928,7 +3927,6 @@ class Service extends REST_Controller
       $gcm_data['type']     = 'group';
       $gcm_data['link_message'] = $link_message;
       $gcm_data['notification_image'] = $notification_image;
-      $gcm_data['filetype'] = $filetype;
       foreach($user_details as $ukey => $uvalue) {
         $notification_status = 'null';
             $gcm_id   = $uvalue['gcm_id'];
@@ -4054,6 +4052,32 @@ class Service extends REST_Controller
             return $this->response(array('status' => "error",'msg' => 'No Keywords Available','error_code' => 101), 404);
         }
    }
+
+
+   //user join group
+    function member_join_to_group_get()
+    {
+
+      $user_id  = $this->get("user_id");
+      $group_id = $this->get("group_id");
+
+      $group    = $this->groups_model->check_unique(array("join_key" => $group_id));
+
+      $result = $this->user_groups_model->check_unique(array("group_id" => $result['id'], "user_id" => $user_id));
+
+         if(count($result) == 0){
+              $ins_data = array();
+              $ins_data['user_id']        = $user_id;
+              $ins_data['group_id']       = $result['id']; 
+              $ins_data['status']         = 1; 
+              $ins_data['is_joined']      = 1; 
+              $ins_data['is_view']        = 1;
+              $ins_data['last_seen_time'] = date('Y-m-d H:i:s');
+              $affected_rows              = $this->user_groups_model->insert($ins_data);
+         }
+
+
+    }
    
 
 }
