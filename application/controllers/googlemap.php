a4c5f335274db4e8a0464eabe4e03c090e8a82e5
Punithalakshmi <punitha.lakshmi87@gmail.com>
2018-05-16 17:12:29 +0530

    -

--- application/controllers/googlemap.php
+++ application/controllers/googlemap.php
@@ -5,6 +5,7 @@ class Googlemap extends CI_Controller {
     function __construct(){
         parent::__construct();
         $this->load->model("group_model");
+        echo "test"; exit;
     }
 
     function index($key = ""){
